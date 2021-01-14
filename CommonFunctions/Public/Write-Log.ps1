18b0a9636af8dc26ab19aa35caf4c2ac243e1778
Calvin Tapp <calvintapp@gmail.com>
2021-01-14 20:35:49 +0000

    Write-Log : Out-File error repaired closes #1

--- CommonFunctions/Public/Write-Log.ps1
+++ CommonFunctions/Public/Write-Log.ps1
@@ -14,6 +14,8 @@ Message that should be printed
 Path to the logfile, should end with .txt or .log
 .PARAMETER AndTerminal
 If provided, will also print the formatted log entry to current execution host instance.
+.PARAMETER DateFormat
+Determines the format for the DateTime stamp to be output in. Default value is dd/MM/yyyy HH:mm:ss
 .EXAMPLE
 Write-Log -Level ERROR -Message "Cannot find the file you specified" -LogFile C:\Logs\log.txt
 Will write the message to the logfile provided
@@ -22,10 +24,13 @@ Will write the message to the logfile provided
 Write-Log -Message "Starting Foo..." -LogFile C:\Logs\FooLog.log -AndTerminal
 Will write "<timestamp> [INFO]  Starting Foo..." to logfile specified and also to screen.
 
+.EXAMPLE
+Write-Log -Message "Starting Foo..." -LogFile C:\Logs\FooLog.txt -DateFormat "yyyy-MM-dd"
+Will write <timestamp> [INFO] Starting Foo... to logfile specified, where <timestamp> will be in stated format
+
 .NOTES
 Work in progress, can definitly be improved...
-    1. Add ability to recieve input from pipe, in order to support redirecting to well formed log
-    2. Invoke some checking to ensure that the logfile is in .txt or .log format    
+    1. Add ability to recieve input from pipe, in order to support redirecting to well formed log      
 #>
 function Write-Log {
     param (
@@ -37,15 +42,22 @@ function Write-Log {
         [string] $Message, 
 
         [Parameter(Mandatory=$true)]
-        [string] $Logfile,
+        [ValidateScript({
+            if(-not ($_ | Test-Path)){ throw "File does not exist."}
+            if(-not ($_ | Test-Path -PathType Leaf)){throw 'The path argument must of a file. Folder paths are not allowed.'}
+            if($_ -notmatch "(\.log|\.txt)"){throw new 'The file specified in the path must be either of type .log or .txt'}
+            return $true
+        })]
+        [System.IO.FileInfo] $Logfile,
+        [switch] $AndTerminal,
 
-        [switch] $AndTerminal
+        [string] $DateFormat = "dd/MM/yyyy HH:mm:ss"
     )
     
-    $timestamp = (Get-Date).ToString("dd/MM/yyyy HH:mm:ss")
+    $timestamp = (Get-Date).ToString($DateFormat)
     $logString = "$timestamp `t[$Level]`t$Message"
 
-    Out-File -FilePath $Logfile -Append $logString
+    $logString | Out-File -FilePath $Logfile -Append
     if($AndTerminal){
         switch ($Level) {
             "WARN" { Write-Host $logString -ForegroundColor DarkYellow }
@@ -53,6 +65,5 @@ function Write-Log {
             "DEBUG" {Write-Host $logString -ForegroundColor Gray}
             Default {Write-Host $logString}
         }
-    }
-    
+    }    
 }
